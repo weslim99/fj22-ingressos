3ff506e355ee168408336ccaf24ad491005a40be
Wesley <lab7780@caelum114-01>
2018-08-06 16:47:05 -0300

    Alterando a Classe Sessao Controller

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -19,7 +19,7 @@ public class SessaoController {
 	
 	@GetMapping("/admin/sessao")
 	public ModelAndView form (@RequestParam("salaId") Integer salaId){
-		ModelAndView modelAndView = new ModelAndView("sessão/sessão");
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 		
 		modelAndView.addObject("sala", salaDao.findOne(salaId));
 		modelAndView.addObject("filmes", filmeDao.findAll());
